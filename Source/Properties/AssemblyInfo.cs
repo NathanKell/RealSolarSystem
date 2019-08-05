d02d5fadd3351c9d8329731f302285bb242b7f03
raidernick <raidernick@users.noreply.github.com>
2019-08-04 23:48:58 -0400

    update for ksp 1.7.x

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.16.0.0")]
-[assembly: AssemblyFileVersion("0.16.2.0")]
+[assembly: AssemblyVersion("0.16.3.0")]
+[assembly: AssemblyFileVersion("0.16.3.0")]
