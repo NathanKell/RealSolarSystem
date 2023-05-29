9a1cdcf7b62bf13801b20825a38676e1ffb76990
NathanKell <nathankell@gmail.com>
2023-05-28 18:04:11 -0700

    Warning dialog for breaking change

---
+++ Source/StartupPopup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace RealSolarSystem
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class StartupPopup : MonoBehaviour
+    {
+        private const string PreferenceFileName = "RSSFinalizeOrbitWarning";
+        private static string PreferenceFilePath => Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            "PluginData",
+            PreferenceFileName);
+
+        public void Start()
+        {
+            if (AssemblyLoader.loadedAssemblies.Any(a => string.Equals(a.name, "ksp_plugin_adapter", StringComparison.OrdinalIgnoreCase)))
+                return;
+
+            if (File.Exists(PreferenceFilePath)) return;
+
+            PopupDialog.SpawnPopupDialog(
+                new Vector2(0, 0),
+                new Vector2(0, 0),
+                new MultiOptionDialog(
+                    "RSSStartupDialog",
+                    "Warning: This version contains breaking changes for non-Principia users. Significant and necessary fixes have been implemented, which may result in planets changing positions in their orbits. Existing maneuvers and vessels in flight may have dramatically altered future encounters. For existing saves, it is recommended to revert to the prior CKAN version and upgrading is not recommended. Please create backups before attempting to update existing saves.",
+                    "Real Solar System",
+                    HighLogic.UISkin,
+                    new DialogGUIVerticalLayout(
+                        new DialogGUIButton("Don't show again", RememberPreference, true),
+                        new DialogGUIButton("Ok", () => { }, true)
+                        )
+                    ),
+            true,
+                HighLogic.UISkin);
+        }
+
+        private static void RememberPreference()
+        {
+            // create empty file
+            File.Create(PreferenceFilePath).Close();
+        }
+    }
+}
